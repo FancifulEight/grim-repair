49e991b07d0f17307dbc3f8900dec3f73dcb17ea
Dungasaur <47060952+Dungasaur@users.noreply.github.com>
2020-02-01 12:47:32 -0700

    Target class and Gamemanager created, adjust SO for soul data.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Game start, set soul materials and target materials.
+
+	//When soul matches target
+
+	//When soul doesn't match target
+}
