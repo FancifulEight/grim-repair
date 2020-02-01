5fc77d350d7299e38cc2511a01cc3686934e1cbf
Dungasaur <47060952+Dungasaur@users.noreply.github.com>
2020-01-31 22:46:24 -0700

    Basic UI works, except for volume sliders.

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+	public string startSceneName;
+	 public GameObject mainPanel, optionsPanel;
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene(startSceneName);
+	}
+
+	public void QuictGame()
+	{
+	#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+	#else
+	      Application.Quit();
+	#endif
+	}
+
+	public void OptionsPanelActivate()
+	{
+		optionsPanel.SetActive(true);
+		mainPanel.SetActive(false);
+	}
+
+	public void OptionsPanelDeactivate()
+	{
+		mainPanel.SetActive(true);
+		optionsPanel.SetActive(false);
+	}
+
+}
