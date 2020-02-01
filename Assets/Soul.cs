a02161e5a27d692a022c22156384ccd0ae0df4aa
Dungasaur <47060952+Dungasaur@users.noreply.github.com>
2020-01-31 23:41:55 -0700

    Trying to set up Soul prefabs for debug

---
+++ Assets/Soul.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soul : MonoBehaviour
+{
+	public ObjectDataScript data;
+	public Color color;
+	public int colorIndex;
+	private void Start()
+	{
+		color = data.colorList[colorIndex];
+	}
+
+
+}
