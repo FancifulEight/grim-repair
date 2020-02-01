ecc51834c50106b4df25d440d1f9bc8c3098f647
FancifulEight <sja2@ualberta.ca>
2020-02-01 11:31:05 -0700

    Added AudioController

---
+++ Assets/AudioController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+    public AudioController ac = null;
+
+    [Header("Audio Sources")]
+    public AudioSource mainSrc;
+    public AudioSource altSrc;
+
+    [Header("Volumes")]
+    public float musicVolume = 1;
+    public float sfxVolume = 1;
+
+    public bool isIntense = false;
+
+    void Awake() {
+        if (ac == null) {
+            ac = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start() {
+        
+    }
+
+    void FixedUpdate() {
+        mainSrc.volume = Mathf.Lerp(mainSrc.volume, musicVolume, Time.fixedDeltaTime);
+        altSrc.volume = Mathf.Lerp(altSrc.volume, ((isIntense) ? musicVolume:0), Time.fixedDeltaTime);
+    }
+}
