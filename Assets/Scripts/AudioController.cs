653a8553b86f124db4d44a95bf3b20e4c1a39fe9
FancifulEight <sja2@ualberta.ca>
2020-02-02 11:34:57 -0700

    Completed Title Fonts

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AudioController : MonoBehaviour {
     public static AudioController ac = null;
@@ -13,7 +14,7 @@ public class AudioController : MonoBehaviour {
     public float musicVolume = 1;
     public float sfxVolume = 1;
 
-    public bool isIntense = false;
+    private bool isIntense = false;
 
     void Awake() {
         if (ac == null) {
@@ -24,16 +25,44 @@ public class AudioController : MonoBehaviour {
         }
     }
 
-    void Start() {
-        
-    }
-
     void FixedUpdate() {
-        mainSrc.volume = Mathf.Lerp(mainSrc.volume, musicVolume, Time.fixedDeltaTime);
-        altSrc.volume = Mathf.Lerp(altSrc.volume, ((isIntense) ? musicVolume:0), Time.fixedDeltaTime);
+        mainSrc.volume = Mathf.Lerp(
+            mainSrc.volume, ((isIntense) ? 0:musicVolume), 2 * Time.fixedDeltaTime);
+        altSrc.volume = Mathf.Lerp(
+            altSrc.volume, ((isIntense) ? musicVolume:0), 2 * Time.fixedDeltaTime);
     }
 
     public void PlaySFX(AudioClip clip, float relativeVol = 1) {
         mainSrc.PlayOneShot(clip, sfxVolume * relativeVol);
+    }
+
+    public void SetIntensity(bool newIntensity) {
+        if (this.isIntense == newIntensity) return;
+
+        if (newIntensity) {
+            StartIntenseMusic();
+        } else {
+            StopIntenseMusic();
+        }
+    }
+
+    public void StartIntenseMusic() {
+        isIntense = true;
+        altSrc.timeSamples = ConvertSamples(
+            mainSrc.timeSamples, mainSrc.clip, altSrc.clip);
+    }
+
+    public void StopIntenseMusic() {
+        isIntense = false;
+        mainSrc.timeSamples = ConvertSamples(
+            altSrc.timeSamples, altSrc.clip, mainSrc.clip);
+    }
+
+    private int ConvertSamples(int otherSamples, AudioClip otherSong, AudioClip mySong) {
+        float otherSampleLength = (otherSong.samples);
+        float mySampleLength = (mySong.samples);
+        
+        int result = (int)((otherSamples * (mySampleLength / otherSampleLength)) % mySampleLength);
+        return result;
     }
 }
