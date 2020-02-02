869d45fbcac60b32497eaf7e45778b351a1cbd17
Dungasaur <47060952+Dungasaur@users.noreply.github.com>
2020-02-02 14:46:05 -0700

    Revert "Revert "GameOverButtonManager Scritp for panel buttons""
    This reverts commit 408833ddc93d16384c770331d6b8fe35eb6852bb.

---
+++ Assets/Scripts/GameOverButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOverButtonManager : MonoBehaviour
+{
+    public void Replay()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void BackToMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}
