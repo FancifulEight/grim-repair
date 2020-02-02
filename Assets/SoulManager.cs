5612003d596c5472ac9954ea5baf387c804546b6
FancifulEight <sja2@ualberta.ca>
2020-02-01 18:25:57 -0700

    Fixed Curtain Controller

--- Assets/SoulManager.cs
+++ Assets/SoulManager.cs
@@ -10,4 +10,32 @@ public class SoulManager : MonoBehaviour {
     public List<Soul> soulOptions = new List<Soul>();
 
     public ObjectDataScript soulData;
+
+    public List<Soul> SetUpGame() {
+        List<Soul> soulList = SetSoulsToPositions();
+
+        SetBody(soulList[Random.Range(0, soulList.Count)].materialIndex);
+
+        return soulList;
+    }
+
+    public List<Soul> SetSoulsToPositions() {
+        List<Soul> soulList = new List<Soul>(soulOptions);
+
+        int index = Random.Range(0, soulList.Count);
+        while(4 < soulList.Count) {
+            soulList.RemoveAt(index);
+            index = Random.Range(0, soulList.Count);
+        }
+
+        return soulList;
+    }
+
+    public void SetBody(int index) {
+
+    }
+
+    public void ChangeSoul() {
+
+    }
 }
