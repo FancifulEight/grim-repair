e3ceffc018b48d72e0a40caffd7c857f1cc48072
FancifulEight <sja2@ualberta.ca>
2020-02-01 19:00:33 -0700

    SoulManager Complete?

--- Assets/SoulManager.cs
+++ Assets/SoulManager.cs
@@ -9,33 +9,58 @@ public class SoulManager : MonoBehaviour {
     public List<Target> targetBodies = new List<Target>();
     public List<Soul> soulOptions = new List<Soul>();
 
-    public ObjectDataScript soulData;
+    public Target currentTarget = null;
+    public List<Soul> currentSouls = new List<Soul>();
 
-    public List<Soul> SetUpGame() {
-        List<Soul> soulList = SetSoulsToPositions();
-
-        SetBody(soulList[Random.Range(0, soulList.Count)].materialIndex);
-
-        return soulList;
+    public void SetUpGame() {
+        SetSoulsToPositions();
+        SetBody(currentSouls[Random.Range(0, currentSouls.Count)].materialIndex);
     }
 
     public List<Soul> SetSoulsToPositions() {
-        List<Soul> soulList = new List<Soul>(soulOptions);
+        ResetSoulPositions();
+        currentSouls = new List<Soul>(soulOptions);
 
-        int index = Random.Range(0, soulList.Count);
-        while(4 < soulList.Count) {
-            soulList.RemoveAt(index);
-            index = Random.Range(0, soulList.Count);
+        int index = Random.Range(0, currentSouls.Count);
+        while(4 < currentSouls.Count) {
+            currentSouls.RemoveAt(index);
+            index = Random.Range(0, currentSouls.Count);
         }
 
-        return soulList;
+        return currentSouls;
+    }
+
+    public void ResetSoulPositions() {
+        foreach (Soul soul in currentSouls) {
+            soul.gameObject.transform.position = new Vector3(0, -100, 0);
+        }
+
+        currentSouls.Clear();
+    }
+
+    public void SetRandomBody() {
+        SetBody(Random.Range(0, targetBodies.Count));
     }
 
     public void SetBody(int index) {
-
+        if (currentTarget != null) 
+            currentTarget.gameObject.transform.position = new Vector3(0, -100, 0);
+        currentTarget = targetBodies[index];
     }
 
-    public void ChangeSoul() {
+    public void ChangeSoul(Soul currentSoul) {
+        ChangeSoul(currentSouls.IndexOf(currentSoul));
+    }
 
+    public void ChangeSoul(int index) {
+        currentSouls[index].gameObject.transform.position = new Vector3(0, -100, 0);
+        currentSouls[index] = GetNewRandomSoul();
+    }
+
+    public Soul GetNewRandomSoul() {
+        List<Soul> soulList = new List<Soul>(soulOptions);
+        soulList.RemoveAll(currentSouls.Contains);
+
+        return soulList[Random.Range(0, soulList.Count)];
     }
 }
