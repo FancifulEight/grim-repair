37aca888830f4056516bb1586f2f207188f0766b
FancifulEight <sja2@ualberta.ca>
2020-02-01 17:15:42 -0700

    Soul Positions

--- Assets/Scripts/ObjectDataScript.cs
+++ Assets/Scripts/ObjectDataScript.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ObjectDataScriptableObject", order = 1)]
 public class ObjectDataScript : ScriptableObject
 {
+	[Header("Soul Materials")]
 	public List<Material> materialList = new List<Material>();
+	public List<Material> soulBodymaterialList = new List<Material>();
+	
+	[Header("Target Body Materials")]
 	public List<Material> targetMaterialList = new List<Material>();
-	public List<Object> bodyList = new List<Object>();
+	[Header("Animal Forms (to use instead of cubes)")]
+	public List<GameObject> bodyList = new List<GameObject>();
 
 	public int RandomMaterialIndex()
 	{
---
+++ Assets/SoulManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulManager : MonoBehaviour {
+    public List<Transform> soulPositions = new List<Transform>();
+    public List<Transform> targetPositions = new List<Transform>();
+
+    public List<Target> targetBodies = new List<Target>();
+    public List<Soul> soulOptions = new List<Soul>();
+
+    public ObjectDataScript soulData;
+}
